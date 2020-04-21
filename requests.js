08a3d2a1c863465106379b59abee7a9399ab06ec
Hugo van der Geest <55272066+HugovanderGeest@users.noreply.github.com>
2020-04-21 13:08:19 +0200

    Add files via upload

--- requests.js
+++ requests.js
@@ -4,10 +4,20 @@ function cityOfChicago(json) {
 
 function covid19(json) {
   console.log(json);
-}
+  let covidconfirmed = json.confirmed;
+  document.getElementById("covid").innerHTML = covidconfirmed;
 
+  let coviddate = json.date;
+  document.getElementById("covid1").innerHTML = coviddate;
+
+  let coviddeaths = json.deaths;
+  document.getElementById("covid2").innerHTML = coviddeaths;
+
+  let covidrecovered = json.recovered;
+  document.getElementById("covid3").innerHTML = covidrecovered;
+}
 var url = "https://data.cityofchicago.org/resource/f7f2-ggz5.json?";
 createGetRequest(url, cityOfChicago);
 
-url = "https://covid-19api.com/api/all-today";
+var url = "https://covid-19api.com/api/all-today";
 createGetRequest(url, covid19);
